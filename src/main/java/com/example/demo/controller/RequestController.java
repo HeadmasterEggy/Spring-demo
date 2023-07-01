62d268f0ae46c70da67c46d4af2a4b6caac101e4
Joey <headmastereggy@gmail.com>
2023-07-01 22:35:41 +0800

    changes

--- src/main/java/com/example/demo/controller/RequestController.java
+++ src/main/java/com/example/demo/controller/RequestController.java
@@ -1,14 +1,51 @@
 package com.example.demo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @RestController
 public class RequestController {
+    //简单参数
     @RequestMapping("/SimpleParam")
-    public String SimpleParam(HttpServletRequest request) {
-        String name = requset.getParameter("name");
+    public String SimpleParam(User user) {
+        System.out.println(user);
+        return "OK";
+    }
 
+    //数组参数
+    @RequestMapping("/ArrayParam")
+    public String ArrayParam(String[] hobby) {
+        System.out.println(Arrays.toString(hobby));
+        return "OK";
+    }
+
+    //日期时间参数
+    @RequestMapping("/dateParam")
+    public String dateParam(@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updateTime) {
+        System.out.println(updateTime.toString());
+        return "OK";
+    }
+
+    //json参数
+    @RequestMapping("/jsonParam")
+    public String jsonParam(@RequestBody User user) {
+        System.out.println(user);
+        return "OK";
+    }
+
+    //路径参数
+    @RequestMapping("/path/{id}")
+    public String pathParam1(@PathVariable Integer id) {
+        System.out.println(id);
+        return "OK";
+    }
+    @RequestMapping("/path/{id}/{name}")
+    public String pathParam2(@PathVariable Integer id, @PathVariable String name) {
+        System.out.println(id + "=" + name);
+        return "OK";
     }
 }
